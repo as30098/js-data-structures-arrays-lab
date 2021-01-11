cb6ad016bdb9a5b3aa7c140454fcf74ea8459560
as30098 <alexspencer421@gmail.com>
2021-01-10 19:42:03 -0500

    commit tree a7261a32085420c242ee4fa0242398d31386e9b1

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+let drivers = [];
+drivers.push("Milo", "Otis", "Garfield");
+
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  let appendArr = drivers.slice();
+  appendArr.push(name);
+  return appendArr;
+}
+
+function prependDriver(name) {
+  let prependArr = drivers.slice();
+  prependArr.unshift(name);
+  return prependArr;
+}
+
+function removeLastDriver() {
+  let removeLastArr = drivers.slice();
+  removeLastArr.pop();
+  return removeLastArr;
+}
+
+function removeFirstDriver() {
+  let removeFirstArr = drivers.slice();
+  removeFirstArr.shift();
+  return removeFirstArr;
+}
